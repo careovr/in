2b5d76ef9131586dc062de92e556a3049ac094f4
careovr <berryboneindia@gmail.com>
2026-06-14 14:40:32 +0530

    la update

--- script/btn.js
+++ script/btn.js
@@ -2,18 +2,18 @@ const featuredItems = [
     { name: 'Instant Appointment', icon: 'img/ico/video-call.png', link: '#', calendly: true },
     { name: 'Doctors', icon: 'img/ico/medical-team.png', link: 'doctors.html' },
     { name: 'Hospitals', icon: 'img/ico/hospital.png', link: 'hospitals.html' },
-    { name: 'Find Labs', icon: 'img/ico/pharmacy.png', link: 'pathlabs.html' },
+    { name: 'Labs', icon: 'img/ico/pharmacy.png', link: 'pathlabs.html' },
     { name: 'Blood Donors', icon: 'img/ico/donors.png', link: 'blood-finder.html' },
     // { name: 'Order Medicine', icon: 'img/ico/medicine.png', link: 'https://gostore.app/berrybone/manualorders' },
-    { name: 'Book Lab Tests', icon: 'img/ico/microscope.png', link: 'https://gostore.app/berrybone/categories/sKh2KDDiBaXSl5G4u0tg' },
+    // { name: 'Book Lab Tests', icon: 'img/ico/microscope.png', link: 'https://gostore.app/berrybone/categories/sKh2KDDiBaXSl5G4u0tg' },
     // { name: 'Book Ambulance', icon: 'img/ico/ambulance.png', link: 'ambulance.html' },
-    { name: 'Caretakers', icon: 'img/ico/old.png', link: 'caregivers.html' },
-    { name: 'My V-Pet', icon: 'img/ico/virtual-pet.png', link: 'my-vpet.html' },
+    // { name: 'Caretakers', icon: 'img/ico/old.png', link: 'caregivers.html' },
+    { name: 'My Health Companion', icon: 'img/ico/virtual-pet.png', link: 'my-vpet.html' },
     { name: 'Breathing Meditation', icon: 'img/ico/yoga.png', link: 'breath.html' },
     { name: 'BMI Calculator', icon: 'img/ico/bmi.png', link: 'bmi-calculator.html' },
     { name: 'BMR Calculator', icon: 'img/ico/bmr.png', link: 'bmr-calculator.html' },
     // { name: 'Custom Diet', icon: 'img/ico/wellbeing.png', link: 'diet-planner.html' },
-    { name: 'Store', icon: 'img/ico/pharmacy.png', link: 'https://gostore.app/berrybone' },
+    // { name: 'Store', icon: 'img/ico/pharmacy.png', link: 'https://gostore.app/berrybone' },
     { name: 'Learning Center', icon: 'img/ico/blogs.png', link: 'blogs.html' },
     // { name: 'Doctor at Home', icon: 'img/ico/doctor-at-home.png', link: 'schedule-doctor-home-visit.html' },
     // { name: 'Vaccination Appointment', icon: 'img/ico/vaccine-record.png', link: 'vaccine-appointment-booking.html' },
@@ -46,7 +46,7 @@ featuredItems.forEach((item, index) => {
     
     button.innerHTML = buttonContent;
 
-    if (index < 7) {
+    if (index < 5) {
         row1.appendChild(button);
     } else {
         row2.appendChild(button);
